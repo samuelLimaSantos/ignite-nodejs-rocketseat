2a2c08f07ce02dcef811cd5e9f5fc86e16609f85
Samuel Santos <samuellima280499@gmail.com>
2021-03-20 04:16:42 -0300

    Finalizando o 4º desafio

---
+++ desafios/desafio04-introducao-ao-SOLID/src/index.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import swagger from "swagger-ui-express";
+
+import { usersRoutes } from "./routes/users.routes";
+import swaggerFile from "./swagger.json";
+
+const app = express();
+
+app.use(express.json());
+
+app.use("/users", usersRoutes);
+
+app.use("/api/v1/doc", swagger.serve, swagger.setup(swaggerFile));
+
+export { app };
---
+++ desafios/desafio04-introducao-ao-SOLID/src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+
+import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
+
+class TurnUserAdminController {
+  constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
+
+  handle(request: Request, response: Response): Response {
+    try {
+      const { user_id } = request.params;
+      const userAdmin = this.turnUserAdminUseCase.execute({ user_id });
+
+      return response.json(userAdmin);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
+  }
+}
+
+export { TurnUserAdminController };
