a6501eec9e055cd42c21f486e74aec6e0236b5d7
Samuel Santos <samuellima280499@gmail.com>
2021-06-14 21:32:10 -0300

    Adicionando controller de rentals

--- aulas/continuando-a-aplicacao/src/modules/rental/infra/typeorm/repositories/RentalsRepository.ts
+++ aulas/continuando-a-aplicacao/src/modules/rental/infra/typeorm/repositories/RentalsRepository.ts
@@ -36,11 +36,17 @@ class RentalsRepository implements IRentalsRepository {
     car_id,
     expected_return_date,
     user_id,
+    id,
+    end_date,
+    total,
   }: ICreateRentalsDTO): Promise<Rental> {
     const rental = this.repository.create({
       car_id,
       expected_return_date,
       user_id,
+      id,
+      end_date,
+      total,
     });
 
     await this.repository.save(rental);
--- aulas/continuando-a-aplicacao/src/shared/infra/http/routes/rentals.routes.ts
+++ aulas/continuando-a-aplicacao/src/shared/infra/http/routes/rentals.routes.ts
@@ -1,12 +1,19 @@
 import { Router } from "express";
 
 import { CreateRentalController } from "@modules/rental/useCases/CreateRental/CreateRentalController";
+import { DevolutionRentalController } from "@modules/rental/useCases/DevolutionRental/DevolutionRentalController";
 
 import { ensureAuthenticated } from "../middlewares/ensureAuthenticated";
 
 const rentalsRoutes = Router();
 const createRentalController = new CreateRentalController();
+const devolutionRentalController = new DevolutionRentalController();
 
 rentalsRoutes.post("/", ensureAuthenticated, createRentalController.handle);
+rentalsRoutes.post(
+  "/devolution/:id",
+  ensureAuthenticated,
+  devolutionRentalController.handle
+);
 
 export { rentalsRoutes };
